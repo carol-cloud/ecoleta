d7e88348af7a4f538d41d1a8b66774f42dde3370
Ana Carolina <karols.15@hotmail.com>
2020-06-10 04:29:15 -0300

    cria página de cadastro | server

---
+++ src/server.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const server = express()
+
+// configurar pasta pública
+server.use(express.static("public"))
+
+// configurar caminhos da minha aplicação
+// página inicial
+// req = requisição
+// res = resposta
+server.get("/", (req, res) => {
+    res.sendFile(__dirname + "/views/index.html")
+
+})
+
+server.get("/create-point", (req, res) => {
+    res.sendFile(__dirname + "/views/create-point.html")
+
+})
+
+
+
+
+
+// ligar o servidor
+server.listen(3000)
