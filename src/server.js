cd7a4efce4d69410b45639bb4e6b2178545d9a33
Ana Carolina <karols.15@hotmail.com>
2020-06-11 02:49:16 -0300

    backend ecoleta

--- src/server.js
+++ src/server.js
@@ -9,17 +9,27 @@ server.use(express.static("public"))
 // req = requisição
 // res = resposta
 server.get("/", (req, res) => {
-    res.sendFile(__dirname + "/views/index.html")
+    return res.render("index.html")
 
 })
 
 server.get("/create-point", (req, res) => {
-    res.sendFile(__dirname + "/views/create-point.html")
+    return res.render("create-point.html")
 
 })
 
+// utilizando template engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
 
+})
 
+server.get("/search", (req, res) => {
+    return res.render("search-results.html")
+
+})
 
 
 // ligar o servidor
